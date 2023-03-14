bd9e888384034cd852fb18a611734f84d63ea450
kakaranara <wahyukoco535@gmail.com>
2023-03-14 15:27:15 +0700

    drawer early setup

--- app/src/main/java/com/kocci/healtikuy/MainActivity.kt
+++ app/src/main/java/com/kocci/healtikuy/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.kocci.healtikuy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.kocci.healtikuy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy{
+    private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.navigationView.setupWithNavController(navController)
+
     }
 }
