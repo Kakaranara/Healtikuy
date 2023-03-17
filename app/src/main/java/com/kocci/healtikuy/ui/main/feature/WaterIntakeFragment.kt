53d91ede63def115914e0f617d24d3acbbc5b930
kakaranara <wahyukoco535@gmail.com>
2023-03-17 15:59:11 +0700

    feat : adapter for water intake (should be optimized + refactor later)

--- app/src/main/java/com/kocci/healtikuy/ui/main/feature/WaterIntakeFragment.kt
+++ app/src/main/java/com/kocci/healtikuy/ui/main/feature/WaterIntakeFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kocci.healtikuy.databinding.FragmentSunExposureBinding
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.kocci.healtikuy.databinding.FragmentWaterIntakeBinding
 
-class WaterIntakeFragment : Fragment() {
+class WaterIntakeFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentWaterIntakeBinding? = null
     private val binding get() = _binding!!
+    var itemCount = 1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +22,34 @@ class WaterIntakeFragment : Fragment() {
     ): View {
         _binding = FragmentWaterIntakeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbarWaterIntake.setupWithNavController(findNavController())
+        setupAdapter()
+
+        binding.button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            binding.button -> {
+                itemCount++
+                val adapter = WaterIntakeAdapter(itemCount)
+                binding.recyclerView.adapter = adapter
+            }
+        }
+    }
+
+    private fun setupAdapter() {
+        val gridLayoutManager = GridLayoutManager(requireActivity(), 4)
+
+        val waterIntakeAdapter = WaterIntakeAdapter(itemCount)
+        binding.recyclerView.apply {
+            adapter = waterIntakeAdapter
+            layoutManager = gridLayoutManager
+        }
     }
 
     override fun onDestroyView() {
