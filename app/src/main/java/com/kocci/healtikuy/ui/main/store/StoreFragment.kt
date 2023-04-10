873d563f1100a33389e32fdcd24c47564937fd1d
kakaranara <wahyukoco535@gmail.com>
2023-04-10 15:12:42 +0700

    store : remove item if it in inventory

--- app/src/main/java/com/kocci/healtikuy/ui/main/store/StoreFragment.kt
+++ app/src/main/java/com/kocci/healtikuy/ui/main/store/StoreFragment.kt
@@ -5,25 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.kocci.healtikuy.core.util.store.CharacterInStore
 import com.kocci.healtikuy.core.util.store.generateCharacterInStore
 import com.kocci.healtikuy.databinding.FragmentStoreBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StoreFragment : Fragment() {
     private var _binding: FragmentStoreBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: StoreViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbarStore.setupWithNavController(findNavController())
-        setupAdapter()
+
+        viewModel.userData.observe(viewLifecycleOwner) {
+            val inventory = it.inventory.toList()
+            val generatedList = generateCharacterInStore()
+            val storeItem = generatedList.filterNot { it.name in inventory }
+
+            setupAdapter(storeItem)
+            binding.tvStoreCoin.text = it.coin.toString()
+        }
 
     }
 
-    private fun setupAdapter() {
-        val list = generateCharacterInStore()
+    private fun setupAdapter(list: List<CharacterInStore>) {
         val mAdapter = StoreAdapter(list, requireActivity())
         val mLayoutManager = GridLayoutManager(requireActivity(), 3)
         binding.rvStore.apply {
--- app/src/main/java/com/kocci/healtikuy/ui/main/store/StoreViewModel.kt
+++ app/src/main/java/com/kocci/healtikuy/ui/main/store/StoreViewModel.kt
@@ -1,7 +1,14 @@
 package com.kocci.healtikuy.ui.main.store
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.kocci.healtikuy.core.domain.usecase.UserUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class StoreViewModel : ViewModel() {
-
+@HiltViewModel
+class StoreViewModel @Inject constructor(
+    userUseCase: UserUseCase
+) : ViewModel() {
+    val userData = userUseCase.getUserData().asLiveData()
 }
