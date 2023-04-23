17d600a4f61e328d10275aebfc48d27eea794142
kakaranara <wahyukoco535@gmail.com>
2023-04-23 20:53:00 +0700

    history improvement

--- app/src/main/java/com/kocci/healtikuy/ui/main/history/HistoryFragment.kt
+++ app/src/main/java/com/kocci/healtikuy/ui/main/history/HistoryFragment.kt
@@ -1,33 +1,48 @@
 package com.kocci.healtikuy.ui.main.history
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ExpandableListView
-import android.widget.Toolbar
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.MaterialToolbar
 import com.kocci.healtikuy.R
+import com.kocci.healtikuy.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment(R.layout.fragment_history) {
 
     //val args
     private val args: HistoryFragmentArgs by navArgs()
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbarHistory)
-        toolbar.setupWithNavController(findNavController())
+        binding.toolbarHistory.setupWithNavController(findNavController())
+
         val list = args.history
         val groupList = list.groupList
         val itemList = list.itemList
 
-        val expandableListView: ExpandableListView = view.findViewById(R.id.elvHistory)
         val adapter = HistoryAccordionAdapter(requireActivity(), groupList, itemList)
+        binding.elvHistory.setAdapter(adapter)
+    }
 
-        expandableListView.setAdapter(adapter)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHistoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
