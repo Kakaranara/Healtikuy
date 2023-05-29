6ebadae96af8dedb47755813c7909f92adf722e6
kakaranara <wahyukoco535@gmail.com>
2023-05-29 11:13:40 +0700

    refactor : changing game rules like points, coin, and strings. todos: update playstore

--- core/src/main/java/com/kocci/healtikuy/core/constant/GameRules.kt
+++ core/src/main/java/com/kocci/healtikuy/core/constant/GameRules.kt
@@ -1,8 +1,8 @@
 package com.kocci.healtikuy.core.constant
 
 object GameRules {
-    const val FIRST_TIME_POINTS: Long = 5000
-    const val MAX_STATUS_POINTS: Long = 30000
-    const val STATUS_COMPLETE_PERCENTAGE : Int = 100
+    const val MAX_STATUS_POINTS: Long = 60000
+    const val FIRST_TIME_POINTS: Long = (0.4 * MAX_STATUS_POINTS).toLong()
+    const val STATUS_COMPLETE_PERCENTAGE: Int = 100
     const val GOALS_WATER_INTAKE = 8
 }
