aac6b1890ab7eb0703dd1124e9c3b7fd1a4f1fb4
kakaranara <wahyukoco535@gmail.com>
2023-06-19 10:58:36 +0700

    refactor : changing system points based on a journal.

--- core/src/main/java/com/kocci/healtikuy/core/constant/GameRules.kt
+++ core/src/main/java/com/kocci/healtikuy/core/constant/GameRules.kt
@@ -1,7 +1,13 @@
 package com.kocci.healtikuy.core.constant
 
 object GameRules {
-    const val MAX_STATUS_POINTS: Long = 60000
+    /**
+     * Max status points are calculated based on a journal about forming a habit.
+     * It needs 66 days to form a habit.
+     * We need to complete all the mission, and the optimal points got is between 2000.
+     * so 2000 * 66 = 132k.
+     */
+    const val MAX_STATUS_POINTS: Long = 132000
     const val FIRST_TIME_POINTS: Long = (0.4 * MAX_STATUS_POINTS).toLong()
     const val STATUS_COMPLETE_PERCENTAGE: Int = 100
     const val GOALS_WATER_INTAKE = 8
--- core/src/main/java/com/kocci/healtikuy/core/util/helper/PointsManager.kt
+++ core/src/main/java/com/kocci/healtikuy/core/util/helper/PointsManager.kt
@@ -1,11 +1,11 @@
 package com.kocci.healtikuy.core.util.helper
 
 object PointsManager {
-    const val EXERCISE_POINT: Long = 500
-    const val WATER_INTAKE_POINT: Long = 500
-    const val SLEEP_POINT: Long = 1000
+    const val EXERCISE_POINT: Long = 400
+    const val WATER_INTAKE_POINT: Long = 400
+    const val SLEEP_POINT: Long = 400
+    const val SUN_EXPOSURE_POINT: Long = 400
     const val NUTRITION_POINT: Long = 60
-    const val SUN_EXPOSURE_POINT: Long = 350
     const val AVOID_POINT : Long = 100
     const val MISSING_POINT : Long = 2000
     const val DAILY_LOGIN_COIN : Int = 8 //? should be refactored since it was not coin manager?
